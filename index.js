728d7c59604809b350468e524b6de7e63e2ba75e
denysiklym <144257831+denysiklym@users.noreply.github.com>
2023-09-12 12:26:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let am = []
+
+let	a = 1
+let b = "Universitat"
+let c = NaN
+let d = true
+let e = "undefined"
+console.log(am)
+
+let me = {
+	name: "Denys",
+	surname: "Klym",
+	age: 17,
+	country: "Ukraine"
+}
+
+console.table(me)
